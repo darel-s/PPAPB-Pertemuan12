ed20a5d6cacdeb83c9a3fddd6427d1678b262efe
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-11-16 09:25:51 +0700

    define database

--- app/src/main/java/com/example/tugaspertemuan12/MainActivity.kt
+++ app/src/main/java/com/example/tugaspertemuan12/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.tugaspertemuan12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tugaspertemuan12.databinding.ActivityMainBinding
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mNoteDao: NoteDao
+    private lateinit var executorService: ExecutorService
+    private var updateId: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        executorService = Executors.newSingleThreadExecutor()
+        val db = NoteRoomDatabase.getDatabase(this)
+        mNoteDao = db!!.noteDao()!!
     }
 }
